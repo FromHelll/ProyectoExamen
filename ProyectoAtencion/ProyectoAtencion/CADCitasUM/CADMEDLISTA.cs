a0fab9563a4781dfe7f0b7f09daf9564ddd1397e
royers <royerschq@gmail.com>
2016-04-27 15:07:55 -0400

    cadcitas proyecto

---
+++ ProyectoAtencion/ProyectoAtencion/CADCitasUM/CADMEDLISTA.cs
@@ -0,0 +1,18 @@
+﻿using CADCitasUM.DSCitasUMTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADCitasUM
+{
+    public class CADMEDLISTA
+    {
+        public static VistaMedicosTableAdapter AdapMedicos = new VistaMedicosTableAdapter();
+        public static DSCitasUM.VistaMedicosDataTable GETMEDICOS()
+        {
+            return AdapMedicos.GetData();
+        }
+    }
+}
---
+++ ProyectoAtencion/ProyectoAtencion/CADCitasUM/CADPermisos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADCitasUM
+{
+    public class CADPermisos
+    {
+        public DataTable tienePermiso(string recurso_id, int usuario_id)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DBCITASConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand cmd = new SqlCommand("VerificarPermisos", con);
+
+            DataTable table = new DataTable();
+            try
+            {
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@recurso_id", recurso_id));
+
+                cmd.Parameters.Add(new SqlParameter("@usuario_id ", usuario_id));
+                SqlDataAdapter miada = new SqlDataAdapter(cmd);
+
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                miada.Fill(table);
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cmd.Parameters.Clear();
+            }
+            return table;
+        }
+    }
+}
